3d34430a7e7da9eee587905673c32ac628dd3023
murtanli <70170580+murtanli@users.noreply.github.com>
2023-09-09 02:42:24 +0300

    Ver 1.7

--- web_site/web_site/urls.py
+++ web_site/web_site/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('main.urls')),
     path('sign_up/', include('register.urls')),
+    path('groups/', include('groups.urls')),
     path('admin/', admin.site.urls),
 ]
